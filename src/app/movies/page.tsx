1faa17a99e09882995bacc03a3b4f7af02044552
lumislu <117718655+lumislu@users.noreply.github.com>
2023-08-08 05:41:44 +0800

    bg

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -2,7 +2,7 @@ import { Carousel, Movies } from "@/components";
 
 const pageMovies = () => {
   return (
-    <div className="pt-20">
+    <div className="pt-20 bg-black">
       <Movies />
     </div>
   );
